5ec96e5bb28744fd7511c2e6c3ab2c1fdec043fd
SunSundr <art.dizigner@gmail.com>
2025-06-29 04:54:07 +0400

    chore: add local start script

---
+++ scripts/connect-k3s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BASTION_IP="56.228.3.177"
+K3S_MASTER_IP="10.0.3.53"
+
+echo "Setting up K3s access..."
+mkdir -p ~/.kube
+scp -i modules/compute/keys/rss-key.pem ec2-user@$BASTION_IP:~/.kube/config ~/.kube/config-k3s
+sed "s/$K3S_MASTER_IP/127.0.0.1/" ~/.kube/config-k3s > ~/.kube/config
+
+echo "Starting SSH tunnel (keep this running)..."
+ssh -i modules/compute/keys/rss-key.pem -L 6443:$K3S_MASTER_IP:6443 ec2-user@$BASTION_IP -N
