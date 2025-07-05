8cac6df62f0e5873653aeb8a361d4edef3c01820
SunSundr <art.dizigner@gmail.com>
2025-07-05 07:52:33 +0400

    feat: add k8s pv & pvc

--- scripts/connect-k3s.sh
+++ scripts/connect-k3s.sh
@@ -7,5 +7,8 @@ mkdir -p ~/.kube
 scp -i modules/compute/keys/rss-key.pem ec2-user@$BASTION_IP:~/.kube/config ~/.kube/config-k3s
 sed "s/$K3S_MASTER_IP/127.0.0.1/" ~/.kube/config-k3s > ~/.kube/config
 
-echo "Starting SSH tunnel (keep this running)..."
-ssh -i modules/compute/keys/rss-key.pem -L 6443:$K3S_MASTER_IP:6443 ec2-user@$BASTION_IP -N
+echo "Starting SSH tunnel with metrics support (keep this running)..."
+ssh -i modules/compute/keys/rss-key.pem \
+  -L 6443:$K3S_MASTER_IP:6443 \
+  -L 10250:$K3S_MASTER_IP:10250 \
+  ec2-user@$BASTION_IP -N
